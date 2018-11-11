86c1b56506cb312b3adbca08c1668b20b12291a7
Daniele Siddi <daniele.siddi@kairosds.com>
2018-11-11 15:44:45 +0100

    Added http call to front

--- front/app.js
+++ front/app.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var app = express();
 
+app.get_users = function() {
+  const https = require('https');
+
+  http.get('http://' + process.env.BACKEND_HOSTNAME + '/users', (resp) => {
+    Console.log(resp);
+  });
+};
+
 app.get('/', function (req, res) {
   res.send('Employees List');
+  app.get_users();
 });
 
 app.listen(3000, function () {
   console.log('App listening on port 3000!');
 });
+
